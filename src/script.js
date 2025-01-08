a8e3ec36d205925bb7fa9b8c7cdc29d48b76e467
cranberrymuffin <aparnalovestocode@gmail.com>
2025-01-08 17:03:06 -0500

    first commit

---
+++ src/script.js
@@ -0,0 +1,56 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/Addons.js'
+
+const cursor = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = -(event.clientY / sizes.height - 0.5)
+})
+
+
+const canvas = document.querySelector('canvas.webgl')
+
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+const camera =  new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 1000)
+camera.position.z = 2
+
+scene.add(camera)
+scene.add(mesh)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+
+
+//Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+
+//Clock
+const clock = new THREE.Clock()
+
+const tick = () => {
+
+    //Update Camera
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
