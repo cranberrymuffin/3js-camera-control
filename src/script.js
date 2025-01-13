d199c6a56f6c1bc57266a612457fe70c0c265373
cranberrymuffin <aparnalovestocode@gmail.com>
2025-01-13 10:26:47 -0500

    make it snow

--- src/script.js
+++ src/script.js
@@ -1,12 +1,29 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/Addons.js'
 
+function generateRandomPointInSphere(radius) {
+    // Generate random values for phi and theta
+    const phi = Math.random() * 2 * Math.PI;
+    const theta = Math.acos(2 * Math.random() - 1);
+  
+    // Generate a random radius value within the sphere
+    const r = Math.cbrt(Math.random()) * radius; 
+  
+    // Convert spherical coordinates to Cartesian coordinates
+    const x = r * Math.sin(theta) * Math.cos(phi);
+    const y = r * Math.sin(theta) * Math.sin(phi);
+    const z = r * Math.cos(theta);
+  
+    return [x, y, z ];
+}
+
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 const camera =  new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 1000)
-
+//uncomment to see globe movement
+// camera.translateZ(10)
 window.addEventListener('resize', ()  => {
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -23,9 +40,13 @@ const particlesGeometry = new THREE.BufferGeometry()
 
 const count = 5000
 const positions = new Float32Array(count * 3)
+const radius = 5
 
-for(let i = 0; i < count * 3; i++) {
-    positions[i] = (Math.random() - 0.5) * 10
+for(let i = 0; i < count; i+=3) {
+    const point = generateRandomPointInSphere(radius)
+    positions[i] = point[0]
+    positions[i+1] = point[1]
+    positions[i+2] = point[2]    
 }
 
 particlesGeometry.setAttribute(
@@ -39,13 +60,8 @@ const particleTexture = textureLoader.load('/textures/snowflake.jpg')
 const particlesMaterial = new THREE.PointsMaterial({size: 0.1, sizeAttenuation: true, map: particleTexture})
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
 
-
-camera.position.z = 2
-
 scene.add(camera)
 scene.add(particles)
-
-const controls = new OrbitControls(camera, canvas)
 
 //Render must be called
 const renderer = new THREE.WebGLRenderer({
@@ -56,6 +72,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 // Render update
 // * * *  makes motion happen
 const animateFrame = () => {
+    particles.rotateX(-0.01)
     renderer.render(scene, camera)
     window.requestAnimationFrame(animateFrame)
 }
